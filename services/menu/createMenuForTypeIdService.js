17081ccc163209c51ab1cf0383d82419d75a55c3
Artem Gursky <h-hfhf-f@mail.ru>
2023-05-29 14:01:22 +0300

    final

--- services/menu/createMenuForTypeIdService.js
+++ services/menu/createMenuForTypeIdService.js
@@ -5,21 +5,23 @@ module.exports = async (req, res) => {
     let { name, type, img, description, price } = req.body;
     price = Number(price);
     type = await Type.findOne({name: type});
-    type = type._id;
-    const menuExists = await Menu.findOne({name: name, type: type});
-    if (menuExists != null) {
-        res.status(409).json({message: 'Такое блюдо уже существует'});
+    if (type) {
+        type = type._id;
+        const menuExists = await Menu.findOne({name: name, type: type});
+        if (menuExists != null) {
+            res.status(409).json({message: 'Такое блюдо уже существует'});
+        } else {
+            const menu = new Menu({
+                name,
+                type,
+                img,
+                description,
+                price
+            })
+            await menu.save()
+                .then(() => res.json({message: 'Блюдо добавлено в меню'}))
+                .catch(e => res.json({message: e.message}));
+        }
     }
-    else {
-        const menu = new Menu({
-            name,
-            type,
-            img,
-            description,
-            price
-        })
-        await menu.save()
-            .then(() => res.json({message: 'Блюдо добавлено в меню'}))
-            .catch(e => res.json({message: e.message}));
-    }
+    else res.status(404).json({message: 'Введите существующий тип'});
 }
